f2752a93a6733fbf2a3e7668b2a83d395b305658
Daniel Leonte <leontedaniel37@gmail.com>
2023-10-30 15:00:26 +0200

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-// import { getImages } from "../utils";
-
 import { useState } from "react";
 import { ImageSlider } from "./components/ImageSlider";
+// import { getImages } from "../utils";
 
 // startDate 5 days ago
 const startDate = new Date(Date.now() - 5 * 24 * 60 * 60 * 1000)
@@ -21,13 +20,14 @@ const tempImages = [
     url: "https://apod.nasa.gov/apod/image/2310/Arp87_HubblePathak_1080.jpg",
   },
   // {
-  //     "copyright": "\nTengyu Cai\n",
-  //     "date": "2023-10-25",
-  //     "explanation": "In 60 seconds, this setting Sun will turn green. Actually, the top of the Sun already appears not only green, but wavey -- along with all of its edges. The Sun itself is unchanged -- both effects are caused by looking along hot and cold layers in Earth's atmosphere.  The unusual color is known as a green flash and occurs because these atmospheric layers not only shift background images but disperse colors into slightly different directions, like a prism.  The featured video was captured earlier this month off the coast of Hawaii, USA. After waiting those 60 seconds, at the video's end, the upper part of the Sun seems to hover alone in space, while turning not only green, but blue.  Then suddenly, the Sun appears to shrink to nothing -- only to return tomorrow.",
-  //     "media_type": "video",
-  //     "service_version": "v1",
-  //     "title": "Gone in 60 Seconds: A Green Flash Sunset",
-  //     "url": "https://www.youtube.com/embed/J3_88eyN44w?rel=0"
+  //   copyright: "\nTengyu Cai\n",
+  //   date: "2023-10-25",
+  //   explanation:
+  //     "In 60 seconds, this setting Sun will turn green. Actually, the top of the Sun already appears not only green, but wavey -- along with all of its edges. The Sun itself is unchanged -- both effects are caused by looking along hot and cold layers in Earth's atmosphere.  The unusual color is known as a green flash and occurs because these atmospheric layers not only shift background images but disperse colors into slightly different directions, like a prism.  The featured video was captured earlier this month off the coast of Hawaii, USA. After waiting those 60 seconds, at the video's end, the upper part of the Sun seems to hover alone in space, while turning not only green, but blue.  Then suddenly, the Sun appears to shrink to nothing -- only to return tomorrow.",
+  //   media_type: "video",
+  //   service_version: "v1",
+  //   title: "Gone in 60 Seconds: A Green Flash Sunset",
+  //   url: "https://www.youtube.com/embed/J3_88eyN44w?rel=0",
   // },
   {
     copyright: "David Cortner",
@@ -89,13 +89,11 @@ const tempImages = [
 
 function App() {
   const [imageIndex, setImageIndex] = useState(0);
-
-  const { title, date, explanation, url, hdurl, media_type, copyright } =
-    tempImages[imageIndex];
+  const { title, explanation } = tempImages[imageIndex];
 
   return (
     <div className="max-w-7xl mx-auto px-4 py-2">
-      <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-y-6">
+      <div className="flex flex-col items-center justify-start min-h-screen py-2 gap-y-6">
         <h2 className="text-3xl font-bold text-center">{title}</h2>
         <ImageSlider
           images={tempImages}
--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -14,38 +14,54 @@ export const ImageSlider = ({
   imageIndex,
   setImageIndex,
 }: ImageSliderProps) => {
+  const { title, date, url, hdurl, media_type, copyright } = images[imageIndex];
+
   const incrementImageIndex = () => {
-    setImageIndex((imageIndex + 1) % images.length);
+    setImageIndex(imageIndex + 1);
   };
 
   const decrementImageIndex = () => {
-    setImageIndex((imageIndex - 1 + images.length) % images.length);
+    setImageIndex(imageIndex - 1);
   };
 
   return (
     <>
-      {images[imageIndex].copyright && (
-        <p className="text-sm text-gray-300 -my-3">
-          Copyright: {images[imageIndex].copyright}
-        </p>
-      )}
+      <p className="text-xs text-gray-300 -my-4 -mb-5">
+        {copyright ? `Copyright: ${copyright}` : <span>&nbsp;</span>}
+      </p>
       <div className="flex flex-col items-center justify-center bg-black gap-1 sm:relative sticky top-2 pb-4">
-        <div className="outline outline-offset-4 outline-2 sm:w-[60%] h-3/6 aspect-square rounded-3xl flex justify-center items-center">
-          <img
-            key={imageIndex}
-            src={images[imageIndex].url} // what if it's a video?
-            alt={images[imageIndex].title}
-          />
+        <div className="outline outline-offset-4 outline-2 sm:w-[60%] aspect-square rounded-3xl flex justify-center items-center">
+          <a href={hdurl || url} target="_blank" rel="noreferrer noopener">
+            <img
+              key={imageIndex}
+              src={url || hdurl}
+              alt={title}
+              className="sm:w-[65rem]"
+            />
+          </a>
         </div>
-        <p className="text- text-gray-300 mt-1 -mb-2">
-          {images[imageIndex].date}
-        </p>
-        <div className="flex justify-between w-full -mt-2 sm:w-3/6">
-          <button className="" onClick={decrementImageIndex}>
-            <BsFillArrowLeftCircleFill className="text-5xl" />
+        <div className="flex justify-between items-center w-full mt-3.5 sm:w-3/6">
+          <button
+            onClick={decrementImageIndex}
+            disabled={imageIndex === 0}
+            className="disabled:opacity-60"
+            style={{ maxWidth: "90%" }}
+          >
+            <BsFillArrowLeftCircleFill className="text-5xl text-[#1d9bf0] bg-white rounded-full" />
           </button>
-          <button className="" onClick={incrementImageIndex}>
-            <BsFillArrowRightCircleFill className="text-5xl" />
+          <p className="text-gray-300">
+            {new Date(date).toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </p>
+          <button
+            onClick={incrementImageIndex}
+            className="disabled:opacity-60"
+            disabled={imageIndex === images.length - 1}
+          >
+            <BsFillArrowRightCircleFill className="text-5xl text-[#1d9bf0] bg-white rounded-full" />
           </button>
         </div>
       </div>
